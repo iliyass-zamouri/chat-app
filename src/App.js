a6b075ad8c2d313e0ebe4b1785ce02034b5ccede
Iliyass ZAMOURI <iliyass.zamouri@gmail.com>
2021-03-05 01:40:16 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
+class App extends Component {
+  render(){
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Login</h1>
+        <div>
+          <input type="text" placeholder="email"></input><br></br>
+          <input type="text" placeholder="password"></input><br></br>
+          <button>Se connecter</button>
+        </div>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/Counter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class Counter extends React.Component {
+    render(){
+        return (
+          <div className="App">
+            <header className="App-header">
+              <h1 className="App-logo">MOROCCO MAROC</h1>
+            </header>
+          </div>
+        );
+    } 
+}
+export default Counter;
